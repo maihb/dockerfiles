a25ee68e53c33ece2af5a0a02197f82af02fc66e
B.M <maihuabao@BMdeMacBook-Pro.local>
2024-06-14 13:54:48 +0800

    去掉 zk 命令行测试

--- kafka/build.sh
+++ kafka/build.sh
@@ -4,10 +4,10 @@ docker-compose up -d
 docker-compose ps
 
 # 创建新主题
-docker exec kafka kafka-topics.sh --create --topic task -zookeeper zookeeper:2181 --partitions 1 --replication-factor 1
+docker exec kafka kafka-topics.sh --create --topic task --bootstrap-server localhost:9092 --partitions 1 --replication-factor 1
 # 列出主题
-docker exec kafka kafka-topics.sh --list --zookeeper zookeeper:2181
+docker exec kafka kafka-topics.sh --list --bootstrap-server localhost:9092
 # 生产消息
-docker exec kafka kafka-console-producer.sh --topic test-topic --broker-list localhost:9092
+docker exec kafka kafka-console-producer.sh --topic task --broker-list localhost:9092
 # 消费消息
-docker exec kafka kafka-console-consumer.sh --topic test-topic --bootstrap-server localhost:9092 --from-beginning
+docker exec kafka kafka-console-consumer.sh --topic task --bootstrap-server localhost:9092 --from-beginning
