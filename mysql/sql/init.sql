e3e54f9f0a0d72c92dbf89449d708937484f03f6
maihuabao <327566368@qq.com>
2024-11-07 11:34:55 +0800

    新增 mysql8 的代码

--- mysql/sql/init.sql
+++ mysql/sql/init.sql
@@ -8,3 +8,8 @@ CREATE DATABASE  `test` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci
 grant all PRIVILEGES on `test`.* to test@'%' identified by '123456';
 flush privileges;
 use test;
+
+-- mysql8 的创建用户
+-- CREATE USER 'newuser'@'%' IDENTIFIED BY 'password123';
+-- GRANT ALL PRIVILEGES ON database_name.* TO 'newuser'@'localhost';
+-- FLUSH PRIVILEGES;
